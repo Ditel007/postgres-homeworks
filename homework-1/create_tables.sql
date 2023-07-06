d154b540d3b73e41c7c2af09f6f52779a994e12e
Nikita Kislykh <kislykhnik@gmail.com>
2023-07-07 00:43:13 +0500

    создание таблиц

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,28 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE customers (
+  customer_id VARCHAR(10) NOT NULL,
+  company_name VARCHAR(100) NOT NULL,
+  contact_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE employees (
+  employee_id SERIAL PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  title VARCHAR(100),
+  birth_date DATE,
+  notes TEXT
+);
+
+
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
+    customer_id VARCHAR(10) NOT NULL,
+    employee_id INTEGER NOT NULL,
+    order_date DATE NOT NULL,
+    ship_city VARCHAR(50) NOT NULL
+);
+
+SELECT * FROM customers;
+SELECT * FROM employees;
+SELECT * FROM orders;
